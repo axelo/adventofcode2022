d00c2a1b832d6955156b75880e21dbeac38f729d
Axel Olsson <axel.olsson@insurello.se>
2022-12-09 09:45:50 +0100

    Day 6

---
+++ 06a.c
@@ -0,0 +1,37 @@
+#include <assert.h>
+#include <limits.h> // UCHAR_MAX
+#include <stdio.h>
+#include <string.h>
+
+#define LINE_CAP 4096
+
+int main(void) {
+    char line[LINE_CAP];
+
+    char *line_start = fgets(line, LINE_CAP, stdin);
+    assert(line_start != NULL && "Expected a line");
+
+    int markers[UCHAR_MAX] = {0};
+    int line_length = strlen(line);
+    int i = 0;
+    int n_tries = 1;
+
+    while (i < line_length) {
+        int marker_index = (int)line[i];
+
+        if (markers[marker_index] == n_tries) {
+            i = n_tries++;
+        } else {
+            if (++i - n_tries == 4 - 1) {
+                break;
+            }
+
+            markers[marker_index] = n_tries;
+        }
+    }
+
+    assert(i != line_length && "Start not found");
+
+    printf("%d\n", i);
+    return 0;
+}
